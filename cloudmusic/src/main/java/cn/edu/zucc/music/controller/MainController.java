fc79f14f90a353292a4a86bfbfccd35c22a2b645
hangzhouwh <hangzhouwh@gmail.com>
2019-12-03 10:58:23 +0800

    注册、登录加md5

--- cloudmusic/src/main/java/cn/edu/zucc/music/controller/MainController.java
+++ cloudmusic/src/main/java/cn/edu/zucc/music/controller/MainController.java
@@ -1,5 +1,6 @@
 package cn.edu.zucc.music.controller;
 
+import cn.edu.zucc.music.Until.MD5Util;
 import cn.edu.zucc.music.Until.Result;
 import cn.edu.zucc.music.Until.ResultStatus;
 import cn.edu.zucc.music.model.Banner;
@@ -12,7 +13,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
+import java.util.TimeZone;
 
 @Controller
 public class MainController {
@@ -26,14 +31,15 @@ public class MainController {
     @CrossOrigin
     @GetMapping(value = "/api/login")
     @ResponseBody
-    public Result<User> getLogin(String userid,String password) {
-        System.out.println(1);
+    public Result<User> getLogin(String userid,String pwd_zero) {
         User user=userService.findById(userid);
-        System.out.println(user.getUserPwd());
+
+        String pwd_md5 = MD5Util.getMD5(pwd_zero);
+
         if (user==null){
             return new Result<>(ResultStatus.USER_NOT_EXISTS);
         }
-        if (user.getUserPwd().equals(password))return new Result<>(ResultStatus.SUCCESS,user);
+        if (user.getUserPwd().equals(pwd_md5))return new Result<>(ResultStatus.SUCCESS,user);
         else
             return new Result<>(ResultStatus.PERMISSION_DENIED);
 
@@ -42,25 +48,37 @@ public class MainController {
     // 注册
     @GetMapping(value = "/api/logup")
     @ResponseBody
-    public JSONObject getLogup(String userName, String password, String password2) {
+    public JSONObject getLogup(String userName, String pwd1, String pwd2) throws ParseException {
         JSONObject jsonObject = new JSONObject();
         User user = userService.findByName(userName);
         if (user!=null){
             jsonObject.put("code", 401); // 用户已经存在
-            jsonObject.put("data", null);
+            jsonObject.put("data", userName);
             return jsonObject;
         }
-        if (password.equals(password2)==false) {
+        if (pwd1.equals(pwd2)==false) {
             jsonObject.put("code", 222); // 密码1 ！= 密码2
             jsonObject.put("data", null);
             return jsonObject;
         }else {
             int maxId = Integer.parseInt(userService.findMaxId());
             String userId = String.valueOf(maxId+1);
+            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+            sdf.setTimeZone(TimeZone.getTimeZone("Asia/Shanghai"));
+            Date date = new Date(); // java.util.Date
+            String dateStr = sdf.format(date);
+            Date dateSql = sdf.parse(dateStr);
+            String pwd = MD5Util.getMD5(pwd1);
+
             User userNew = new User();
             userNew.setUserId(userId);
             userNew.setUserName(userName);
-            userNew.setUserPwd(password);
+            userNew.setUserPwd(pwd);
+            userNew.setAvatarUrl("https://i.loli.net/2019/12/03/MpqVU45Hi8JwzY9.jpg"); // 默认头像
+            userNew.setUserType(1); // 0-管理员 1-普通用户 2-评论用户
+            userNew.setCreateTime(dateSql);
+
+
             userService.addUser(userNew);
             jsonObject.put("code", 200); // SUCCESS
             jsonObject.put("data", userId);
